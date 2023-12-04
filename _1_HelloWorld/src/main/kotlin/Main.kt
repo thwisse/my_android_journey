26f30818bce4b651461c3a3730d9ffb6a88397f6
thwisse <thwisse@gmail.com>
2023-12-04 11:56:37 +0300

    day 1

---
+++ _1_HelloWorld/src/main/kotlin/Main.kt
@@ -0,0 +1,48 @@
+fun main()
+{
+    println("Hello World!")
+
+    print("Learn ")
+    println("Kotlin")
+
+    // not al
+
+    /*
+    not al
+    not al
+    */
+
+    println(35+34)
+    println("69")
+
+    // * asteris operatoru ile carpim yapiyoruz.
+    println(3 * 5)
+
+    ////////////////////
+    // var - val
+
+    var sayi1 = 15
+    sayi1 = 16
+    var isim1 = "oguzhan"
+
+    val sehir1 = "hatay"
+    // val degiskenlerin degeri daha sonra degistirilemez.
+    //sehir1 = "maras"
+
+    ////////////////////////
+    // int float double
+
+    val a:Int = 5
+    // float'in sonunda F gelmek zorunda. double ile ayirt edilebilmesi icin. F yoksa double'dir.
+    val b:Float = 6.14F
+    // float double'in aksine kusuratli olmak zorunda degildir.
+    val b2:Float = 6F
+    val c:Double = 7.567
+
+    var today: String
+    today = "Sunday"
+
+
+
+
+}
